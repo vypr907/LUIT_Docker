aaa09dfd209d15d0cf5de68a855eaf1abc7071b0
vypr907 <steven.laszloffy@gmail.com>
2022-07-09 16:02:44 +0000

    python script to create file with time/date of docker container creation

---
+++ basic/deployDate.py
@@ -0,0 +1,8 @@
+import os
+from datetime import datetime
+
+with open("deployment.txt","w+") as file: #"w+" means the file will be overwritten each time
+    file.write("This container was deployed at %s." %datetime.now())
+    file.close()
+    print(file)
+    
