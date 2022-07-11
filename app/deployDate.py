b3b9dd09bc9928c60b93687c8011853ac9d0d707
vypr907 <steven.laszloffy@gmail.com>
2022-07-11 08:45:16 +0000

    completed complex steps

--- app/deployDate.py
+++ app/deployDate.py
@@ -1,8 +1,6 @@
-import os
+#!usr/bin/env python3
 from datetime import datetime
 
 with open("deployment.txt","w+") as file: #"w+" means the file will be overwritten each time
     file.write("This container was deployed at %s." %datetime.now())
-    file.close()
-    print(file)
-    
+    file.close
