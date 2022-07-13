a539c7483ec5c117d5c9ea9df3f57f5d78022118
vypr907 <steven.laszloffy@gmail.com>
2022-07-13 16:50:46 -0500

    adding latest coursework files

---
+++ infrastructure/dev1/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+import random
+import os
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return jsonify({"data": {"container" : os.getenv('HOSTNAME'), "device": os.getenv("DEVICE"), "temp" : random.randrange(-10,21,1)}})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
